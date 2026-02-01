5b10caa326bdc83cc3076ad9e19c3777ee99aa28
ragii11 <ragii62996@gmail.com>
2026-02-01 23:55:58 +0530

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -18,40 +18,33 @@ class Paper {
   rotating = false;
 
   init(paper) {
-    paper.addEventListener('touchmove', (e) => {
-      e.preventDefault();
-      if(!this.rotating) {
-        this.touchMoveX = e.touches[0].clientX;
-        this.touchMoveY = e.touches[0].clientY;
-        
-        this.velX = this.touchMoveX - this.prevTouchX;
-        this.velY = this.touchMoveY - this.prevTouchY;
-      }
-        
-      const dirX = e.touches[0].clientX - this.touchStartX;
-      const dirY = e.touches[0].clientY - this.touchStartY;
-      const dirLength = Math.sqrt(dirX*dirX+dirY*dirY);
-      const dirNormalizedX = dirX / dirLength;
-      const dirNormalizedY = dirY / dirLength;
+    paper.addEventListener(
+  'touchmove',
+  (e) => {
+    e.preventDefault();
 
-      const angle = Math.atan2(dirNormalizedY, dirNormalizedX);
-      let degrees = 180 * angle / Math.PI;
-      degrees = (360 + Math.round(degrees)) % 360;
-      if(this.rotating) {
-        this.rotation = degrees;
-      }
+    if (!this.holdingPaper) return;
 
-      if(this.holdingPaper) {
-        if(!this.rotating) {
-          this.currentPaperX += this.velX;
-          this.currentPaperY += this.velY;
-        }
-        this.prevTouchX = this.touchMoveX;
-        this.prevTouchY = this.touchMoveY;
+    this.touchMoveX = e.touches[0].clientX;
+    this.touchMoveY = e.touches[0].clientY;
 
-        paper.style.transform = `translateX(${this.currentPaperX}px) translateY(${this.currentPaperY}px) rotateZ(${this.rotation}deg)`;
-      }
-    })
+    this.velX = this.touchMoveX - this.prevTouchX;
+    this.velY = this.touchMoveY - this.prevTouchY;
+
+    this.currentPaperX += this.velX;
+    this.currentPaperY += this.velY;
+
+    this.prevTouchX = this.touchMoveX;
+    this.prevTouchY = this.touchMoveY;
+
+    paper.style.transform = `
+      translate(${this.currentPaperX}px, ${this.currentPaperY}px)
+      rotate(${this.rotation}deg)
+    `;
+  },
+  { passive: false }
+);
+
 
     paper.addEventListener('touchstart', (e) => {
       if(this.holdingPaper) return; 
